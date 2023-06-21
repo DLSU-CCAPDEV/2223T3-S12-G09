60452d4b874dd7b20d23146ca03c59a853662d7b
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-06-21 08:03:11 +0800

    Added nav bar

---
+++ js/nav.js
@@ -0,0 +1,20 @@
+/*
+    <span class="hnav">
+        <a href="register.html"
+        target="_blank">
+        Register
+        </a>
+    </span>
+
+    <span class="hnav">Login</span>
+ */
+
+const links = [
+    //    link             html
+    ["register.html", "Register"],
+    ["login.html", "Login"],
+];
+
+function loadLinks() {
+
+}
