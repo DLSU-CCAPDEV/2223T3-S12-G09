e4ad3bc88ed8845fb3a18090bc2473fa59fd7a70
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-06-26 12:32:12 +0800

    Added null radio value check

--- js/reserve.js
+++ js/reserve.js
@@ -4,7 +4,7 @@ const seatH1 = document.createElement("span");
 const labH1 = document.createElement("span");
 
 reserveDiv.appendChild(reserveH1);
-reserveH1.appendChild(document.createTextNode("Reserve seat "));
+reserveH1.appendChild(document.createTextNode("Reserve Seat "));
 reserveH1.appendChild(seatH1);
 reserveH1.appendChild(document.createTextNode(" at Lab "));
 reserveH1.appendChild(labH1);
@@ -14,30 +14,12 @@ reserveDiv.style.display = "none";
 seatH1.id = "chosen-seat";
 labH1.id = "chosen-lab";
 
-// function test(obj) {
-//     alert(obj.idx + ", " + obj.lab);
-//     reserveDiv.style.display = "block";
-//     seatH1.innerText = obj.idx;
-//     labH1.innerText = obj.lab;
-// }
-
-// function test(labIdx, seatIdx) {
-//     alert(labIdx + ", " + seatIdx);
-//     reserveDiv.style.display = "block";
-//     seatH1.innerText = seatIdx;
-//     labH1.innerText = labIdx;
-// }
-
-// function test(obj) {
-//     alert(obj.idx);
-//     obj.style.backgroundColor = "red";
-//     reserveDiv.style.display = "block";
-//     seatH1.innerText = seatIdx;
-//     labH1.innerText = labIdx;
-// }
 function showReserve(labform) {
     const radioForm = labform["lab-radio"];
     const radioValue = radioForm.value;
+
+    if (radioValue == "")
+        return;
 
     alert(radioValue);
     console.log(radioValue);
