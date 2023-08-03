9d5574ca1248ea64a3e4f055fce8e3d02885a2af
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-08-03 15:18:17 +0800

    Make route for checking account and viewing seat

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,8 @@
 const controllers = require('../controllers/controller');
 const labControllers = require('../controllers/labController');
-const reserveControllers= require('../controllers/reserveController');
+const reserveControllers = require('../controllers/reserveController');
+const accountControllers = require('../controllers/accountController');
+const seatControllers = require('../controllers/seatController');
 
 const express = require('express');
 const app = express();
@@ -9,6 +11,8 @@ app.get('/', controllers.getIndex);
 app.get('/labs', labControllers.getLabs);
 
 app.get('/checkReservation', reserveControllers.checkReservation);
+app.get('/getAccount', accountControllers.getAccount);
+app.get('/viewSeat', )
 app.post('/makeReservation', reserveControllers.makeReservation);
 
 module.exports = app;
