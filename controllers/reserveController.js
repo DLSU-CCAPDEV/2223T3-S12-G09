d864abb41b676224bf80e047a85684e7c45171b4
jjg-git <john_joseph_giron@dlsu.edu.ph>
2023-08-04 22:23:57 +0800

    Simplified data gathering and added listReservations

--- controllers/reserveController.js
+++ controllers/reserveController.js
@@ -3,24 +3,38 @@ const Reservation = require('../models/ReservationModel.js');
 
 const reserveController = {
     checkReservation: async function (req, res) {
-        var seat = req.query.seat_id;
-        var user = req.query.user;
-        var lab = req.query.lab;
-        var date_reserved = req.query.date_reserved;
-        var reservation_date = req.query.reservation_date;
-        var time_slot = req.query.time_slot;
-
-        var query = {
-            seat_id: seat,
-            // user: user,
-            lab: lab,
-            // date_reserved: date_reserved,
-            // reservation_date: reservation_date,
-            time_slot: time_slot
-        };
+        // var seat = req.query.seat_id;
+        // var user = req.query.user;
+        // var lab = req.query.lab;
+        // // var date_reserved = req.query.date_reserved;
+        // var reservation_date = req.query.reservation_date;
+        // var time_slot = req.query.time_slot;
+        //
+        // var query = {
+        //     seat_id: seat,
+        //     // user: user,
+        //     lab: lab,
+        //     // date_reserved: date_reserved,
+        //     reservation_date: reservation_date,
+        //     time_slot: time_slot
+        // };
 
         // console.log(query);
-        var result = await db.findOne(Reservation, query);
+        var result = await db.findOne(Reservation, req.query);
+        console.log(result);
+        res.send(result);
+    },
+
+    listReservations: async function (req, res) {
+        // var date_reserved = req.query.date_reserved;
+        // var reservation_date = req.query.reservation_date;
+        // var time_slot = req.query.time_slot;
+
+        // console.log("query: ");
+        console.log(req);
+        var result = await db.findMany(Reservation, req.query);
+        console.log("listReservations(): ");
+        // console.log(req.query);
         console.log(result);
         res.send(result);
     },
