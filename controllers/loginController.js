6397ebcb244a2b6d02d67a738f16ab53789d573f
ReyObejero <rey_obejero@dlsu.edu.ph>
2023-08-06 01:29:19 +0800

    Sessions

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -10,31 +10,34 @@ const loginController = {
     postLogIn: async function (req, res) {
         var username = req.body.username;
         var password = req.body.password;
-
         var result = await db.findOne(User, {username: username}, "");
 
         if(result){
             var user = {
                 username: result.username,
-                email: result.email,
                 type: result.type
             };
 
             bcrypt.compare(password, result.password, function(err, equal){
                 if(equal){
-                    req.session.username = user.username,
-                    req.session.email = user.email;
+                    req.session.username = user.username;
                     req.session.type = user.type;
-                    console.log("session email: " + req.session.email); //remove later
-                    res.redirect('/profile/' + user.username);
-                }else{
-                    console.log("password incorrect"); //remove later
-                    var details = {error: "Username and/or Password is incorrect."};
+                    res.redirect('/');
+                } else{
+                    var details = {
+                        flag: false,
+                        error: "Username and/or Password is incorrect."
+                    };
+
                     res.render("login", details);
                 }
             });
         } else{
-            var details = {error: "Username and/or Password is incorrect."};
+            var details = {
+                flag: false,
+                error: "Username and/or Password is incorrect."
+            };
+
             res.render("login", details);
         }
     }
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,6 @@ const controllers = require('../controllers/controller.js');
 const labControllers = require('../controllers/labController.js');
 const reserveControllers = require('../controllers/reserveController.js');
 const signupController = require('../controllers/signupController.js');
-const successController = require('../controllers/successController.js');
 const loginController = require("../controllers/loginController.js");
 
 const express = require('express');
@@ -21,9 +20,6 @@ app.get('/checkEmail', signupController.checkEmail);
 app.get('/checkUsername', signupController.checkUsername);
 app.get('/signup', signupController.getSignUp);
 app.post('/signup', validation.signupValidation(), signupController.postSignUp);
-//app.post('/signup', signupController.postSignUp);
-
-//app.get('/success', successController.getSuccess);
 
 app.get('/login', loginController.getLogin);
 app.post('/login', loginController.postLogIn);
