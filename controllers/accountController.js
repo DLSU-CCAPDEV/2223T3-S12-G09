5e754494e0f5fd37df3d31c82980d4aa06f64d6c
ReyObejero <rey_obejero@dlsu.edu.ph>
2023-08-06 22:18:20 +0800

    Added profile picture to profile page

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -5,23 +5,16 @@ const Session = require('./sessionController.js');
 const accountController = {
     getProfile: async function (req, res) {
         var query = {username: req.params.username};
-        var currentUser = {username: req.session.username};
-        var projection = 'fname lname username description';
         var details = Session.connectSession(req, res);
         var isCurrentUser = false;
 
-        // if (req.session.username) {
-        //     details.flag = true;
-        //     details.username = true;
-        // } else
-        //     details.flag = false;
-
-        var result = await db.findOne(User, query, projection);
+        var result = await db.findOne(User, query);
 
         if(result !== null){
             details.fname = result.fname;
             details.lname = result.lname;
             details.username_url = result.username;
+            details.pfpURL = result.pfpURL;
             details.description = result.description;
 
             if(query.username === req.session.username)
